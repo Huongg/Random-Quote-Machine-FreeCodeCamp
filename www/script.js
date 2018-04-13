eb37632574b125215867fb35d2575fbe25f4c389
Huongg <32060364+Huongg@users.noreply.github.com>
2018-04-13 19:01:50 +0100

    HTML & CSS layout

---
+++ www/script.js
@@ -0,0 +1,39 @@
+let QUOTESJSON = [
+    {
+      "quote": "Without Sunday, I wouldn't know when to put on the brakes of a hurtling life.",
+      "author": "Byron Pulsifer"
+    },
+    {
+      "quote": "Nothing pains some people more than having to think.",
+      "author": "Martin Luther"
+    },
+    {
+      "quote": "I've learned that you can tell a lot about a person by the way (s)he handles these three things: a rainy day, lost luggage, and tangled Christmas tree lights.",
+      "author": "Maya Angelou "
+    },
+    {
+      "quote": "When science finally locates the center of the universe, some people will be surprised to learn they're not it.",
+      "author": "Bernard Bailey "
+    },
+    {
+      "quote": "When fate hands us a lemon, let's try to make lemonade.",
+      "author": "Dale Carnegie"
+    },
+    {
+      "quote": "I know God will not give me anything I can't handle. I just wish He didn't trust me so much. ",
+      "author": "Mother Teresa"
+    },
+    {
+      "quote": "Eliminating procrastination from our lives is like trying to stop a moving train; it's not easy.",
+      "author": "Joseph R. Ferrari"
+    },
+    {
+      "quote": "The only time I don't have any problems in this world is when I am already six feet below the ground.",
+      "author": "James Jason"
+    },
+    {
+      "quote": "For some, life is like a river. It becomes overpowering, uncontrollable and sometimes wild.",
+      "author": "Ravindranath Jayaratne"
+    }
+    
+]
